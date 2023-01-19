94367f84347ac03d18521272b73e4bcd708c1de0
beuleu <billyklotz789@gmail.com>
2023-01-19 23:07:30 +0100

    show, close taskForm on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import {TasksList} from "./components/TasksList";
-import { TaskFormModal } from "./components/TaskFormModal";
+import {TaskFormModal} from "./components/TaskFormModal";
 import {Header} from "./components/Header";
 import {data} from "./data/tasks";
 
 const tasks = data
 
 const App = () => {
-  const title = "To do list";
-  const taskToEdit: any = null;
+    const title = "To do list";
+    const taskToEdit: any = null;
+    const [showForm, setShowForm] = useState(false)
 
-  const updateTaskState = (taskId: number) => {
-    console.error("I need to be implemented");
-  };
+    const updateTaskState = (taskId: number) => {
+        console.error("I need to be implemented");
+    };
 
-  const addOrEditTask = (event: any, taskToEditId?: number) => {
-    event.preventDefault();
-    console.error("I need to be implemented");
-  };
+    const addOrEditTask = (event: any, taskToEditId?: number) => {
+        event.preventDefault();
+        console.error("I need to be implemented");
+    };
 
-  const editTask = (taskId: number) => {
-    console.error("I need to be implemented");
-  };
+    const editTask = (taskId: number) => {
+        console.error("I need to be implemented");
+    };
 
-  const deleteTask = (taskId: number) => {
-    console.error("I need to be implemented");
-  };
+    const deleteTask = (taskId: number) => {
+        console.error("I need to be implemented");
+    };
 
-  return (
-    <div className="main">
-      <Header titre={title}/>
+    return (
+        <div className="main">
+            <Header titre={title}/>
 
-      <TasksList list={data}/>
-      <button
-        className="add-task-btn"
-        onClick={() => console.log("this button should open the modal")}
-      >
-        +
-      </button>
-      <TaskFormModal
-        show={false}
-        handleClose={() =>
-          console.log("pass me a method that will close the modal")
-        }
-        addOrEditTask={addOrEditTask}
-        initialValues={
-          taskToEdit != null
-            ? {
-                id: taskToEdit.id,
-                title: taskToEdit.title,
-                description: taskToEdit.description,
-              }
-            : undefined
-        }
-      />
-    </div>
-  );
+            <TasksList list={data}/>
+            <button
+                className="add-task-btn"
+                onClick={() => {
+                    setShowForm(true)
+                }}
+            >
+                +
+            </button>
+            <TaskFormModal
+                show={showForm}
+                handleClose={() => setShowForm(false)}
+                addOrEditTask={addOrEditTask}
+                initialValues={
+                    taskToEdit != null
+                        ? {
+                            id: taskToEdit.id,
+                            title: taskToEdit.title,
+                            description: taskToEdit.description,
+                        }
+                        : undefined
+                }
+            />
+        </div>
+    );
 };
 
 export default App;
